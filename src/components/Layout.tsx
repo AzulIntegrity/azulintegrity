82a00ea40c49a5c216200aeec4999bac0d10112b
scarletlvuyyuru <slvuyyuru@hotmail.com>
2025-10-30 09:59:32 -0400

    update to tags and bugs

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,7 @@
 "use client";
 import React, { useState, useEffect} from "react";
+import Link from "next/link";
+import Image from "next/image";
 interface HeaderProps {
   children: React.ReactNode;
   className?: string;
@@ -31,32 +33,28 @@ export function Footer({ children, className = '' }: FooterProps) {
         <div className="footerGrid">
           <div className="footerCol">
             <h4>Quick Links</h4>
-            
-            <a href="/">Home</a>
-            <a href="/services">Services</a>
-            <a href="/contact">Contact</a>
+            <Link href="/">Home</Link>
+            <Link href="/services">Services</Link>
+            <Link href="/contact">Contact</Link>
           </div>
           <div className="footerCol">
             <h4>Contact</h4>
-         
-            <span> <img src="/emailIcon.png" alt="Email" className="contactIcon" /> <a href="mailto:info@azulintegrity.com">info@azulintegrity.com</a></span>
-            <span> <img src="/phoneIcon.png" alt="Phone" className="contactIcon" /> <a href="tel:+119044764732">(904) 476-4732</a></span>
+            <span> <Image src="/emailIcon.png" alt="Email" className="contactIcon" width={20} height={20} /> <a href="mailto:info@azulintegrity.com">info@azulintegrity.com</a></span>
+            <span> <Image src="/phoneIcon.png" alt="Phone" className="contactIcon" width={20} height={20} /> <a href="tel:+119044764732">(904) 476-4732</a></span>
             <div className="footerSocial">
               <a href="https://www.linkedin.com/company/azul-integrity-accounting-services/" target="_blank" rel="noopener noreferrer">
-                <img src="/linkedInIcon.png" alt="LinkedIn" className="socialIcon" />
+                <Image src="/linkedInIcon.png" alt="LinkedIn" className="socialIcon" width={20} height={20} />
               </a>
               <a href="https://www.facebook.com/people/Azul-Integrity-Accounting-Services-LLC/61581436237919/#" target="_blank" rel="noopener noreferrer">
-                <img src="/facebookIcon.png" alt="Facebook" className="socialIcon" />
+                <Image src="/facebookIcon.png" alt="Facebook" className="socialIcon" width={20} height={20} />
               </a>
             </div>
           </div>
           <div className="footerCol">
             <h4>Legal Pages</h4>
-          
-            <a href="/terms">Terms of Service</a>
-            <a href="/privacy">Privacy Policy</a>
-          
-            <a href="/sitemap">Site Map</a>
+            <Link href="/terms">Terms of Service</Link>
+            <Link href="/privacy">Privacy Policy</Link>
+            <Link href="/sitemap">Site Map</Link>
           </div>
         </div>
         <div className="footer-copyright">
